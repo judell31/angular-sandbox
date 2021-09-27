fa084163e4ad7911d0c4ae760874f3e8922e3a9a
Justin <justintdavis8@gmail.com>
2021-09-27 09:09:50 -0500

    Removed todo item

--- src/app/services/path.guard.ts
+++ src/app/services/path.guard.ts
@@ -8,8 +8,7 @@ export class PathGuard implements CanActivate {
   constructor(
     private router: Router
   ){}
-
-  //Todo: not sure if i need all of the types or can use void somehow
+  
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): any {
     if (this.router.url === "/") {
       this.router.navigate(["/home"]).then(() => {})
