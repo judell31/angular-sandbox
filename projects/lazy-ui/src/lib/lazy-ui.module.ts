fdd89f22c4b7669118d1f14c9b98ad607d804156
Justin Davis <justintdavis8@gmail.com>
2021-12-06 08:54:22 -0600

    Update lazy-ui.module.ts

--- projects/lazy-ui/src/lib/lazy-ui.module.ts
+++ projects/lazy-ui/src/lib/lazy-ui.module.ts
@@ -2,16 +2,16 @@ import {NgModule} from '@angular/core';
 import {LazyInputComponent} from "./components/inputs/lazy-input/lazy-input.component";
 import {CommonModule} from "@angular/common";
 import {FormGroupDirective, FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {NumbersDirective} from "./directives/numbers/numbers.directive";
 import {PhoneNumberDirective} from "./directives/phone-number/phone-number.directive";
 import {LzyButtonComponent} from "./components/buttons/lzy-button/lzy-button.component";
 import {LzyEmailDirective} from "./directives/lzy-email/lzy-email.directive";
-import { LzyNextStepperButtonComponent } from './components/buttons/lzy-next-stepper-button/lzy-next-stepper-button.component';
-import { LzyBackStepperButtonComponent } from './components/buttons/lzy-back-stepper-button/lzy-back-stepper-button.component';
 import {MaterialModule} from "./material.module";
 import {LazyPhoneInputComponent} from "./components/inputs/lazy-phone-input/lazy-phone-input.component";
-import {LazyNumberInputComponent} from "./components/inputs/lazy-number-input/lazy-number-input.component";
+import {NumbersDirective} from "./directives/numbers/numbers.directive";
 import {LazyDropDownComponent} from "./components/inputs/lazy-drop-down/lazy-drop-down.component";
+import {LzyNextStepperButtonComponent} from "./components/buttons/lzy-next-stepper-button/lzy-next-stepper-button.component";
+import {LzyBackStepperButtonComponent} from "./components/buttons/lzy-back-stepper-button/lzy-back-stepper-button.component";
+import {LazyNumberInputComponent} from "./components/inputs/lazy-number-input/lazy-number-input.component";
 
 @NgModule({
   declarations: [
@@ -24,7 +24,7 @@ import {LazyDropDownComponent} from "./components/inputs/lazy-drop-down/lazy-dro
     LazyNumberInputComponent,
     LazyDropDownComponent,
     LzyNextStepperButtonComponent,
-    LzyBackStepperButtonComponent,
+    LzyBackStepperButtonComponent
   ],
   imports: [
     CommonModule,
@@ -39,9 +39,11 @@ import {LazyDropDownComponent} from "./components/inputs/lazy-drop-down/lazy-dro
     LazyInputComponent,
     LzyEmailDirective,
     LazyPhoneInputComponent,
-    LazyNumberInputComponent,
     LazyDropDownComponent,
     LzyButtonComponent,
+    LazyNumberInputComponent,
+    PhoneNumberDirective,
+    NumbersDirective,
     LzyNextStepperButtonComponent,
     LzyBackStepperButtonComponent,
   ]
