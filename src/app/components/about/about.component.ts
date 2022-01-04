bc969cff57699f0d09b8838b9f0783301b225933
Justin <justintdavis8@gmail.com>
2022-01-03 22:26:23 -0600

    Added bootstrap grid

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-about',
@@ -6,10 +7,42 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+  inputFormGroup: FormGroup;
+  arrayList: any[];
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.inputForm();
+
+    this.arrayList = [
+      {
+        name: "Item 1"
+      },
+      {
+        name: "Item 2"
+      },
+      {
+        name: "Item 3"
+      },
+      {
+        name: "Item 4"
+      },
+      {
+        name: "Item 5"
+      },
+      {
+        name: "Item 6"
+      }
+    ]
   }
 
+  inputForm() {
+    this.inputFormGroup = this.formBuilder.group({
+      email: new FormControl(''),
+      firstName: new FormControl('')
+    });
+  }
 }
