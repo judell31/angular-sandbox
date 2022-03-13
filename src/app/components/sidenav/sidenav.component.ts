ebc56afd16bda93f0a377befeacd8760e8dfdb5c
Justin Davis <justintdavis8@gmail.com>
2022-03-13 14:36:44 -0500

    Added toggle when sidenav item is clicked

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -15,4 +15,29 @@ export class SidenavComponent implements OnInit {
   ngOnInit(): void {
     this.navItems = this.navMenu.navItems;
   }
+
+  //FixMe: have to double click the button twice to get the sidebar back
+  hideSideNav() {
+    document.getElementById("offcanvas")!
+      .classList.remove("show");
+
+    setTimeout(() => {
+      document.getElementById("offcanvas")!
+        .setAttribute("style", "visibility:hidden;");
+    }, 100);
+
+    document.getElementsByClassName("offcanvas-backdrop")[0].remove();
+
+    document.getElementsByTagName("body")[0]
+      .removeAttribute("style");
+
+    document.getElementById("offcanvas")!
+      .setAttribute("aria-hidden", "true");
+
+    document.getElementById("offcanvas")!
+      .removeAttribute("aria-modal");
+
+    document.getElementById("offcanvas")!
+      .removeAttribute("role");
+  }
 }
